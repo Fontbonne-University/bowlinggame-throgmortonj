0177393181853284f361e0b1e2a86f21bd847877
throgmortonj <43552446+throgmortonj@users.noreply.github.com>
2019-03-28 15:17:12 -0500

    Add files via upload
    finished tests

--- BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
+++ BowlingGame/Bowling/src/BowlingGame/BowlingTest.java
@@ -2,20 +2,83 @@ package BowlingGame;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingTest {
+	private Game g;
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception{
+		g = new Game();
+	}
+	
+	private void rollMany(int n, int pins) {
+		for(int i=0; i< n; i++) {
+			g.roll(pins);
+			
+		}
+	}
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+	
+	private void rollStrike() {
+		g.roll(10);
+	}
+	
+	@After 
+	public void tearDown() throws Exception{
+		
+	}
+	
+	@Test public void canRoll() {
+		
+		g.roll(0);
+		
+	}
+	
+	@Test
+	public void gutterGame() {
+		rollMany(20,0);
+		assertEquals(0, g.score());
+	}
+	
+	
+	
+	@Test
+	public void allOnes() {
+		
+		rollMany(20,1);
+		assertEquals(20, g.score());
+	}
 
+	@Ignore
+	@Test
+	public void oneSpare() {
+		rollSpare(); // spare
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16,g.score());
 	}
 
 	@Test
-	public void canCreateGame()
-	{
-		Game g = new Game();
+	public void oneStrike() {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, g.score());
 	}
+
+	@Test
+	public void perfectGame() {
+		rollMany(12, 10);
+		assertEquals(300, g.score());
+	}
+	
+	
 }
--- BowlingGame/Bowling/src/BowlingGame/Game.java
+++ BowlingGame/Bowling/src/BowlingGame/Game.java
@@ -1,6 +1,33 @@
 package BowlingGame;
 
 public class Game {
+	private int score = 0;
+	private int rolls[] = new int[21];
+	private int currentRoll = 0;
+	public void roll(int pins) {
+		// TODO Auto-generated method stub
+		rolls[currentRoll ++] = pins;
+		
+	}
 
+	public int score() {
+		// TODO Auto-generated method stub
+		int firstInFrame = 0;
+		for(int frame=0; frame<10; frame++) {
+			if(rolls[firstInFrame] == 10) {
+				score +=10 +rolls[firstInFrame+1] + rolls[firstInFrame+2];
+				firstInFrame+=1;
+			}
+			else if(rolls[firstInFrame] + rolls[firstInFrame+1] == 10) {
+				score +=10 + rolls[firstInFrame+2];
+				firstInFrame+=2;
+			}
+			else {
+				score += rolls[firstInFrame] + rolls[firstInFrame+1];
+				firstInFrame+=2;
+			}
+		}
+		return score;
+	}
 
 }
